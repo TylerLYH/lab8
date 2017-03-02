f1bb0470eb270e9a6abbbacd225dc46a55ddbffa
Tyler Li <Tyler Li>
2017-03-01 23:24:52 -0800

    fixing

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,10 +1,10 @@
 
 function initMap() {
   // Create center marker at UCSD
-  var ucsd_ltlng = {lat:32.885014, lng:-117.241194};
+   var ucsd_ltlng = {lat:32.885014, lng:-117.241194};
 
   // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById('map'), {
     center: ucsd_ltlng,
     zoom: 15
   });
